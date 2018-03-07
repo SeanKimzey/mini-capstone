2d5f4ccbd60691d49097f990bcd45a27d91ebac6
Sean Kimzey <sean.kimzey@gmail.com>
2018-03-07 17:07:10 -0600

    added frontend file that requires unirest to access API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/all-products' => 'products#every_product'
+  get '/product_1' => 'products#product_one'
+  
 end
